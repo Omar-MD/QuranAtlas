91c341e5f3a9a8b14413d4fc4f2b5f97c208eaa7
Omar Duadu <omarduadu@gmail.com>
2026-04-04 16:33:33 +0100

    chore: add Storybook base config with html-vite framework

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+  stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/html-vite',
+    options: {},
+  },
+  staticDirs: ['../public'],
+}
+
+export default config
